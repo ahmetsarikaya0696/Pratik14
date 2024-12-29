d17323a8449aa7cc2eb7111f2806a27cebb03eb6
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-12-30 02:57:33 +0300

    Add project files.

---
+++ Pratik14/Araba.cs
@@ -0,0 +1,28 @@
+﻿namespace Pratik14
+{
+    public class Araba
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+
+        private int kapiSayisi;
+
+        public int KapiSayisi
+        {
+            get { return kapiSayisi; }
+            set
+            {
+                if (value != 2 || value != 4)
+                {
+                    Console.WriteLine("Kapı sayısı 2 veya 4 olmalıdır!");
+                    kapiSayisi = -1;
+                    return;
+                }
+
+                kapiSayisi = value;
+            }
+        }
+
+    }
+}
---
+++ Pratik14/Program.cs
@@ -0,0 +1,13 @@
+﻿using Pratik14;
+
+Araba araba = new Araba()
+{
+    Marka = "Tesla",
+    Model = "Model S",
+    KapiSayisi = 1,
+    Renk = "Kırmızı"
+};
+// Kapı sayısı 2 veya 4 olmalıdır! yazacaktır
+
+Console.WriteLine($"Kapi sayisi : {araba.KapiSayisi}"); // Kapi sayisi : -1 yazacaktır
+
